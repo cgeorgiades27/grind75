63e4d7981865869c216ef7948ee43aac3f862f55
Chris Georgiades <cg@csq.dev>
2023-08-25 15:42:45 -0400

    completed week 2, question 1

---
+++ week2/first_bad_version/first_bad_version_test_cases.go
@@ -0,0 +1,11 @@
+package firstbadversion
+
+var TestCases = []struct {
+	input  int
+	output int
+}{
+	{input: 5, output: 4},
+	{input: 1, output: 1},
+	{input: 3, output: 1},
+	{input: 4, output: 4},
+}
