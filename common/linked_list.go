d59044e7b9a14edcfcf011ce0de30f74bcfa6cf6
Chris Georgiades <cg@csq.dev>
2023-11-21 22:09:53 -0500

    added C implementation of llist

--- common/linked_list.go
+++ common/linked_list.go
@@ -1,5 +1,47 @@
 package common
 
+/*
+#include <stdlib.h>
+
+typedef struct ListNode
+{
+	struct ListNode* next;
+	int val;
+} ListNode;
+
+
+ListNode* array_to_list(int *arr, int size)
+{
+	if (size == 0)
+		return NULL;
+
+	ListNode* head = malloc(sizeof(ListNode));
+	head->val = arr[0];
+	ListNode* curr = head;
+
+	for (int i = 1; i < size; ++i)
+	{
+		curr->next = malloc(sizeof(ListNode));
+		curr = curr->next;
+		curr->val = arr[i];
+	}
+
+	curr->next = NULL;
+
+	return head;
+}
+*/
+import "C"
+import "unsafe"
+
+func SliceToListC(slc []int) *ListNode[int] {
+	size := len(slc)
+	arr := (*C.int)(unsafe.Pointer(&slc[0]))
+
+	head := C.array_to_list(arr, C.int(size))
+	return (*ListNode[int])(unsafe.Pointer(head))
+}
+
 // Definition for singly-linked list.
 type ListNode[T any] struct {
 	Val  T
--- week1/merge_sorted_lists/merge_sorted_lists_cg.go
+++ week1/merge_sorted_lists/merge_sorted_lists_cg.go
@@ -2,6 +2,72 @@ package mergesortedlists
 
 import "github.com/cgeorgiades27/grind75/common"
 
+/*
+#include <stdlib.h>
+
+typedef struct ListNode {
+	struct ListNode* next;
+	int val;
+} ListNode;
+
+ListNode* merge_sorted_lists(ListNode* l1, ListNode* l2)
+{
+	ListNode* curr = NULL;
+	ListNode* final = NULL;
+
+	ListNode* itr1 = list1;
+	ListNode* itr2 = list2;
+
+	while (itr1 && itr2)
+	{
+		ListNode* temp = NULL;
+		if (itr1->val < itr2->val)
+		{
+			temp = itr1;
+			itr1 = itr1->next;
+		}
+		else
+		{
+			temp = itr2;
+			itr2 = itr2->next;
+		}
+
+		// first look
+		if (!final)
+		{
+			final = temp;
+			curr = temp;
+		}
+		else
+		{
+			curr->next = temp;
+			curr = curr->next;
+		}
+	}
+
+	// drain remaining lists
+	if (itr1)
+	{
+		if (!final)
+			final->next = itr1;
+		else
+			curr->next = itr1;
+	}
+
+	if (itr2)
+	{
+		if (!final)
+			final->next = itr2;
+		else
+			curr->next = itr2;
+	}
+
+	// head of merged list
+	return final;
+}
+
+*/
+
 func mergeSortedListsCG(list1, list2 *common.ListNode[int]) *common.ListNode[int] {
 	var current, final *common.ListNode[int]
 	itr1, itr2 := list1, list2
