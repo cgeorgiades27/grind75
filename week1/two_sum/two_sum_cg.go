f7002578e4bfcc85e06cd592a2178d5304823f86
Chris Georgiades <cg@csq.dev>
2024-01-14 11:22:35 -0500

    fix two sum

--- week1/two_sum/two_sum_cg.go
+++ week1/two_sum/two_sum_cg.go
@@ -1,12 +1,12 @@
 package twosum
 
 func twoSumCg(target int, nums []int) []int {
-	m := make(map[int]int, len(nums))
+	m := make(map[int]int)
 	for i, num := range nums {
 		if v, ok := m[target-num]; ok {
 			return []int{v, i}
 		}
-		m[num-target] = i
+		m[num] = i
 	}
 	return []int{}
 }
